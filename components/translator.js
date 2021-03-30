923e55b15b0682c0942877d6eb1174f8ed147bbe
Esraa Abdelhady <esraaabdou94@gmail.com>
2021-03-30 03:16:59 +0100

    add translator logic

--- components/translator.js
+++ components/translator.js
@@ -4,7 +4,70 @@ const americanToBritishTitles = require("./american-to-british-titles.js")
 const britishOnly = require('./british-only.js')
 
 class Translator {
-
+    constructor(text, locale) {
+        this.text = text;
+        this.locale = locale;
+    }
+    get translatedText() {
+        return this.getTranslatedText();
+    }
+    getTranslatedText() {
+        const textArr = this.text.split(" ");
+        textArr[0] = textArr[0].charAt(0).toUpperCase() + textArr[0].slice(1);
+        let translationArr = [];
+        if(this.locale == "american-to-british") {
+            translationArr = textArr.map(word => {
+                const lowerCasedWord = word.toLowerCase();
+                if(americanOnly[lowerCasedWord]) {
+                    const returning =  americanOnly[lowerCasedWord];
+                    return `<span class="highlight">${returning}</span>`
+                }
+                else if(americanToBritishSpelling[lowerCasedWord]) {
+                    const returning = americanToBritishSpelling[lowerCasedWord];
+                    return `<span class="highlight">${returning}</span>`
+                }
+                else if(americanToBritishTitles[lowerCasedWord]) {
+                    const title =  americanToBritishTitles[lowerCasedWord];
+                    const returning = title.charAt(0).toUpperCase() + title.slice(1);
+                    return `<span class="highlight">${returning}</span>`
+                }
+                
+                for (const property in americanToBritishTitles) {
+                    if(americanToBritishTitles[property] == lowerCasedWord) {
+                        const returning = lowerCasedWord.charAt(0).toUpperCase() + lowerCasedWord.slice(1);
+                        return `<span class="highlight">${returning}</span>`
+                    }
+                }
+                return word;
+            });
+        } else if (this.locale == "british-to-american"){
+            translationArr = textArr.map(word => {
+                const lowerCasedWord = word.toLowerCase();
+                if(britishOnly[lowerCasedWord]) {
+                    const returning = britishOnly[lowerCasedWord];
+                    return `<span class="highlight">${returning}</span>`
+                }
+                if(americanToBritishTitles[lowerCasedWord]) {
+                    const returning = lowerCasedWord.charAt(0).toUpperCase() + lowerCasedWord.slice(1);
+                    return `<span class="highlight">${returning}</span>`
+                }
+                for (const property in americanToBritishTitles) {
+                    if(americanToBritishTitles[property] == lowerCasedWord) {
+                        const returning = property.charAt(0).toUpperCase() + property.slice(1);
+                        return `<span class="highlight">${returning}</span>`
+                    }
+                }
+                for (const property in americanToBritishSpelling) {
+                    if(americanToBritishSpelling[property] == lowerCasedWord) {
+                        const returning = property;
+                        return `<span class="highlight">${returning}</span>`
+                    }
+                }
+                return word;
+            });
+        }
+        return translationArr.join(" ");
+    }
 }
 
 module.exports = Translator;
--- routes/api.js
+++ routes/api.js
@@ -8,6 +8,26 @@ module.exports = function (app) {
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      const text = req.body.text;
+      const locale = req.body.locale;
+      if(!req.body.hasOwnProperty('text') || !req.body.hasOwnProperty('locale')) {
+        res.send({"error": "Required field(s) missing"});
+      } else if(locale !== "american-to-british" && locale !== "british-to-american") {
+        res.send({"error": "Invalid value for locale field"});
+      } else if(text) {
+        const translator = new Translator(text, locale);
+        let translation = '';
+        if(translator.translatedText === text) {
+          translation = "Everything looks good to me!";
+        } else {
+          translation = translator.translatedText;
+        }
+        res.send({
+          "text": text,
+          "translation": translation
+        });
+      } else {
+        res.send({"error": "No text to translate"});
+      }
     });
 };
