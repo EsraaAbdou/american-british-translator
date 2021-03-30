0727531b4d69aa5a2191ba716d9d3e425a887406
Esraa Abdelhady <esraaabdou94@gmail.com>
2021-03-30 22:50:25 +0100

    edit translate to british logic with passing tests

--- components/translator.js
+++ components/translator.js
@@ -12,39 +12,48 @@ class Translator {
         return this.getTranslatedText();
     }
     getTranslatedText() {
-        const textArr = this.text.split(" ");
-        textArr[0] = textArr[0].charAt(0).toUpperCase() + textArr[0].slice(1);
-        let translationArr = [];
+        let translation = this.text.charAt(0).toUpperCase() + this.text.slice(1);
         if(this.locale == "american-to-british") {
-            translationArr = textArr.map(word => {
-                const lowerCasedWord = word.toLowerCase();
-                const timeRegex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
-                if(timeRegex.test(word)) {
-                    const returning = word.replace(':', '.');
-                    return `<span class="highlight">${returning}</span>`
-                }
-                if(americanOnly[lowerCasedWord]) {
-                    const returning =  americanOnly[lowerCasedWord];
-                    return `<span class="highlight">${returning}</span>`
-                }
-                else if(americanToBritishSpelling[lowerCasedWord]) {
-                    const returning = americanToBritishSpelling[lowerCasedWord];
-                    return `<span class="highlight">${returning}</span>`
-                }
-                else if(americanToBritishTitles[lowerCasedWord]) {
-                    const title =  americanToBritishTitles[lowerCasedWord];
-                    const returning = title.charAt(0).toUpperCase() + title.slice(1);
-                    return `<span class="highlight">${returning}</span>`
-                }
-                
-                for (const property in americanToBritishTitles) {
-                    if(americanToBritishTitles[property] == lowerCasedWord) {
-                        const returning = lowerCasedWord.charAt(0).toUpperCase() + lowerCasedWord.slice(1);
-                        return `<span class="highlight">${returning}</span>`
-                    }
-                }
-                return word;
+
+            let matchedPropertyArr = [];
+            for (const property in americanOnly) {
+                if(translation.toLowerCase().includes(property)) matchedPropertyArr.push(property);
+            }
+            replaceMatch(americanOnly);
+   
+            matchedPropertyArr = [];
+            for (const property in americanToBritishSpelling) {
+                if(translation.toLowerCase().includes(property)) matchedPropertyArr.push(property);
+            }
+            replaceMatch(americanToBritishSpelling);
+
+            matchedPropertyArr = [];
+            for (const property in americanToBritishTitles) {
+                if(translation.toLowerCase().includes(property) || translation.toLowerCase().includes(americanToBritishTitles[property]))
+                    matchedPropertyArr.push(property);
+            }
+            replaceMatch(americanToBritishTitles, true);
+
+            const timeRegex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
+            const timeStrings = translation.split(" ").filter(d => timeRegex.test(d) == true);
+            const newTimeStrings = timeStrings.map(element => element.replace(":", "."));
+            timeStrings.forEach((element, idx) => {
+                translation = translation.replace(element, `<span class="highlight">${newTimeStrings[idx]}</span>`);
             });
+
+            return translation;
+
+            function replaceMatch(obj, titleFlag = false) {
+                if(matchedPropertyArr.length > 0) {
+                    const property = matchedPropertyArr.sort((a, b) => b.length - a.length)[0];
+                    const firstIdx = translation.toLowerCase().search(property);
+                    const secondIdx = firstIdx + property.length;
+                    const toBeRepalced = translation.slice(firstIdx, secondIdx);
+                    let replacement = obj[property];
+                    if(titleFlag) replacement = replacement.charAt(0).toUpperCase() + replacement.slice(1);
+                    translation = translation.replace(toBeRepalced, `<span class="highlight">${replacement}</span>`);
+                }
+            }
         } else if (this.locale == "british-to-american"){
             translationArr = textArr.map(word => {
                 const lowerCasedWord = word.toLowerCase();
@@ -76,7 +85,7 @@ class Translator {
                 return word;
             });
         }
-        return translationArr.join(" ");
+        return translation;
     }
 }
 
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -4,5 +4,61 @@ const assert = chai.assert;
 const Translator = require('../components/translator.js');
 
 suite('Unit Tests', () => {
+    test('Translate Mangoes are my favorite fruit to British English', function () {
+        const translator = new Translator("Mangoes are my favorite fruit", "american-to-british");
+        assert.strictEqual(translator.translatedText, 'Mangoes are my <span class="highlight">favourite</span> fruit')
+    });
+
+    test('Translate I ate yogurt for breakfast to British English', function () {
+        const translator = new Translator("I ate yogurt for breakfast.", "american-to-british");
+        assert.strictEqual(translator.translatedText, 'I ate <span class="highlight">yoghurt</span> for breakfast.')
+    });
+
+    test("Translate We had a party at my friend's condo to British English", function () {
+        const translator = new Translator("We had a party at my friend's condo.", "american-to-british");
+        assert.strictEqual(translator.translatedText, 'We had a party at my friend\'s <span class="highlight">flat</span>.')
+    });
+
+    test('Translate Can you toss this in the trashcan for me? British English', function () {
+        const translator = new Translator("Can you toss this in the trashcan for me?", "american-to-british");
+        assert.strictEqual(translator.translatedText, 'Can you toss this in the <span class="highlight">bin</span> for me?')
+    });
+
+    test('Translate The parking lot was full to British English', function () {
+        const translator = new Translator("The parking lot was full.", "american-to-british");
+        assert.strictEqual(translator.translatedText, 'The <span class="highlight">car park</span> was full.')
+    });
+
+    test('Translate Like a high tech Rube Goldberg machine to British English', function () {
+        const translator = new Translator("Like a high tech Rube Goldberg machine.", "american-to-british");
+        assert.strictEqual(translator.translatedText, 'Like a high tech <span class="highlight">Heath Robinson device</span>.')
+    });
+
+    test('Translate To play hooky means to skip class or work to British English', function () {
+        const translator = new Translator("To play hooky means to skip class or work.", "american-to-british");
+        assert.strictEqual(translator.translatedText, 'To <span class="highlight">bunk off</span> means to skip class or work.')
+    });
+
+    test('Translate No Mr. Bond, I expect you to die to British English', function () {
+        const translator = new Translator("No Mr. Bond, I expect you to die.", "american-to-british");
+        assert.strictEqual(translator.translatedText, 'No <span class="highlight">Mr</span> Bond, I expect you to die.')
+    });
+
+    test('Translate Dr. Grosh will see you now to British English', function () {
+        const translator = new Translator("Dr. Grosh will see you now.", "american-to-british");
+        assert.strictEqual(translator.translatedText, '<span class="highlight">Dr</span> Grosh will see you now.')
+    });
+
+    test('Translate Lunch is at 12:15 today to British English', function () {
+        const translator = new Translator("Lunch is at 12:15 today.", "american-to-british");
+        assert.strictEqual(translator.translatedText, 'Lunch is at <span class="highlight">12.15</span> today.')
+    });
+
+    // test('Translate  to American English', function () {
+    //     const translator = new Translator("", "british-to-american");
+    //     assert.strictEqual(translator.translatedText, "")
+    // });
+
+
 
 });
